e263c4c99b76b1cdb136b094cbc7640345224da7
Ochiponu-Awodi <awodiomale@gmail.com>
2024-10-06 09:51:04 +0100

    React Router 6 Tutorial Introduction

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Vans from './pages/Vans.jsx'
+import VanDetail from './pages/VanDetail.jsx'
+
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter>
+        <header>
+          <Link to="/" className="site-logo" >#VANLIFE</Link>
+          <nav>
+            <Link to="/about">About</Link>
+            <Link to="/vans">Vans</Link>
+          </nav>
+        </header>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/vans' element={<Vans />} />
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Routes>
+    </BrowserRouter>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Vans.jsx
@@ -0,0 +1,40 @@
+/* eslint-disable */
+
+import createServer from "../../Server"
+import { useEffect, useState } from "react"
+import './Vans.css'
+import { Link } from "react-router-dom"
+
+function Vans () {
+    const [vans, setVans] = useState ([])
+
+    useEffect (() => {
+        fetch('/api/vans')
+            .then(res => res.json())
+            .then(data => setVans(data.vans))
+    }, [])
+
+    const vanElements = vans.map(van => (
+        <div key={van.id} className="van-tile">
+            <Link to={`/vans/${van.id}`} aria-label={`View details for ${van.name}, priced at $${van.price} per day`}>
+                <img src={van.imageUrl} alt={`Image of ${van.name}`} />
+                <div className="van-info">
+                    <p className="van-name">{van.name}</p>
+                    <p>${van.price}<span>/day</span></p>
+                </div>
+                <i className={`van-type ${van.type} selected`}>{van.type}</i>
+            </Link>
+        </div>
+    ))
+
+    return (
+        <div className="van-list-container">
+            <h1>Explore our van options</h1>
+            <div className="van-list">
+                {vanElements}
+            </div>
+        </div>
+    )
+}
+
+export default Vans
